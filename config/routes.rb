3984752544b2481d168d248d64109a97c943f5ee
Aki Suzuki <akisuzucky@gmail.com>
2014-05-31 22:13:37 -0700

    finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :microposts
+
+  resources :users
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
